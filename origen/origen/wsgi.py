8be0b00b088a4ad740c45c1ca03ea082e88f4916
Josep Maria Salvia Hornos <josh.salvia@gmail.com>
2020-11-28 13:27:27 +0100

    Added all files for first version. Warning: requires redis

---
+++ origen/origen/wsgi.py
@@ -0,0 +1,22 @@
+"""
+WSGI config for origen project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'origen.settings')
+
+# application = get_wsgi_application()
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    # Just HTTP for now. (We can add other protocols later.)
+})
